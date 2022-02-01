0885c3ae87b22a291d6e1f4758f6c5779a482158
czarmisha <vertigomisha@gmail.com>
2022-02-02 00:42:04 +0500

    asssss

--- wedding/settings.py
+++ wedding/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'wedding.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '0809',
+        'HOST': 'localhost',
+        # 'PORT': '8000',
     }
 }
 
