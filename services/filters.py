b2ef781184613bfed61c9501a42195532acd2a92
Bobur <you@example.com>
2022-02-24 11:30:31 +0500

    aww

--- services/filters.py
+++ services/filters.py
@@ -22,29 +22,22 @@ class VideographerFilter(django_filters.FilterSet):
 
 
 class RestaurantFilter(django_filters.FilterSet):
-    average_check_gt = django_filters.NumberFilter(
-        field_name='average_check', lookup_expr='gte')
-    average_check_lt = django_filters.NumberFilter(
-        field_name='average_check', lookup_expr='lte')
-    capacity_gt = django_filters.NumberFilter(
-        field_name='capacity', lookup_expr='gte')
-    capacity_lt = django_filters.NumberFilter(
-        field_name='capacity', lookup_expr='lte')
+    price = django_filters.RangeFilter(field_name='average_check')
+    capacity = django_filters.RangeFilter()
     type = django_filters.ModelChoiceFilter(
-        queryset=models.RestaurantType.objects.all())
+        queryset=models.RestaurantType.objects.all(), empty_label='Тип заведения')
     location = django_filters.ModelMultipleChoiceFilter(
         queryset=District.objects.all(),
         widget=forms.SelectMultiple,
     )
     kitchen = django_filters.ModelMultipleChoiceFilter(
         queryset=models.KitchenType.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.SelectMultiple,
     )
 
     class Meta:
         model = models.Restaurant
-        fields = ['type', 'average_check_gt', 'average_check_lt',
-                  'capacity_gt', 'capacity_lt', 'location']
+        fields = ['type', 'price', 'capacity', 'location']
 
 
 class RegistryOfficeFilter(django_filters.FilterSet):
@@ -88,27 +81,23 @@ class DecorFilter(django_filters.FilterSet):
 
 class TransportFilter(django_filters.FilterSet):
     type = django_filters.ChoiceFilter(
-        field_name='type', choices=models.Transport._TYPE)
+        field_name='type', choices=models.Transport._TYPE, empty_label='Тип')
     location = django_filters.ModelMultipleChoiceFilter(
         queryset=District.objects.all(),
         widget=forms.SelectMultiple,
     )
     car_type = django_filters.ModelChoiceFilter(
-        queryset=models.CarType.objects.all())
+        queryset=models.CarType.objects.all(), empty_label='Тип авто')
     car_brand = django_filters.ModelMultipleChoiceFilter(
         queryset=models.CarBrand.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.SelectMultiple,
     )
-    price_lt = django_filters.NumberFilter(
-        field_name='price', lookup_expr='lte')
-    price_gt = django_filters.NumberFilter(
-        field_name='price', lookup_expr='gte')
+    price = django_filters.RangeFilter()
     with_driver = django_filters.BooleanFilter(field_name='with_driver')
 
     class Meta:
         model = models.Transport
-        fields = ['type', 'location', 'price_gt', 'price_lt',
-                  'car_type', 'car_brand', 'with_driver']
+        fields = ['type', 'location', 'price', 'car_type', 'car_brand', 'with_driver']
 
 
 class PresenterFilter(django_filters.FilterSet):
@@ -134,25 +123,17 @@ class PresenterFilter(django_filters.FilterSet):
 
 
 class MusicFilter(django_filters.FilterSet):
-    price_lt = django_filters.NumberFilter(
-        field_name='price', lookup_expr='lte')
-    price_gt = django_filters.NumberFilter(
-        field_name='price', lookup_expr='gte')
-    price_per_evening_lt = django_filters.NumberFilter(
-        field_name='price_per_evening', lookup_expr='lte')
-    price_per_evening_gt = django_filters.NumberFilter(
-        field_name='price_per_evening', lookup_expr='gte')
+    price = django_filters.RangeFilter(field_name='price_per_evening')
     composition = django_filters.ChoiceFilter(
-        field_name='composition', choices=models.Music._COMPOSITION_TYPE)
-    vocal = django_filters.ChoiceFilter(
-        field_name='vocal', choices=models.Music._VOCAL_TYPE)
-    language = django_filters.ModelChoiceFilter(
-        queryset=models.Language.objects.all())
+        field_name='composition', choices=models.Music._COMPOSITION_TYPE, empty_label='Исполнители')
+    language = django_filters.ModelMultipleChoiceFilter(
+        queryset=models.Language.objects.all(),
+        widget=forms.SelectMultiple
+        )
 
     class Meta:
         model = models.Music
-        fields = ['price_per_evening_gt', 'price_per_evening_lt',
-                  'price_gt', 'price_lt', 'composition', 'vocal', 'language']
+        fields = ['price', 'composition', 'language']
 
 
 class ArtistFilter(django_filters.FilterSet):
