08efa1dd67dbb4e32ea85f87cd658cb40ce09e5c
Bobur <you@example.com>
2022-03-15 11:49:12 +0500

    asdasdas

--- wedding/views.py
+++ wedding/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from tender.models import Tender
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 
 
 def home(request):
@@ -8,7 +10,16 @@ def home(request):
     return render(request, 'wedding/home.html', context={'tenders': tenders, 'tender_count': tender_count})
 
 def promo(request):
-    return render(request, 'wedding/promo.html', context={})
+    if request.method == 'POST':
+        spec_name = request.POST.get('service_user_pk')
+        spec_phone = request.POST.get('client_user_pk')
+        messages.error(request, 'Форма не отправлена. Попробуйте позже.')
+        return render(request, 'wedding/promo.html', context={
+            'form': False,
+            'text': _('Форма отправлена, спасибо! Администратор свяжется с Вами в ближайшее время'),
+            })
+    else:
+        return render(request, 'wedding/promo.html', context={'form': True})
 
 def catalog(request):
     return render(request, 'services/catalog.html', context={})
