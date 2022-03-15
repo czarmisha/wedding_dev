27eb5c8035d283aedc165b28bd27bcccce04fc0b
Bobur <you@example.com>
2022-03-15 12:19:21 +0500

    asdasdas

--- wedding/views.py
+++ wedding/views.py
@@ -1,3 +1,4 @@
+import telebot
 from django.shortcuts import render
 from tender.models import Tender
 from django.contrib import messages
@@ -11,9 +12,20 @@ def home(request):
 
 def promo(request):
     if request.method == 'POST':
-        spec_name = request.POST.get('service_user_pk')
-        spec_phone = request.POST.get('client_user_pk')
-        messages.error(request, 'Форма не отправлена. Попробуйте позже.')
+        spec_name = request.POST.get('name')
+        spec_phone = request.POST.get('phone')
+        bot_token = '5164361466:AAFFuHU1szO-4MQKjcKK5aVM_TBCDxJVqzg'
+        chat_id = '-703685470'
+        txt = f'Новый специиалист хочет зарегистрироваться \n\n <b>Имя</b>: {spec_name} \n <b>Телефон</b>: {spec_phone}'
+        bot = telebot.TeleBot(bot_token)
+        try:
+            bot.send_message(
+            chat_id=chat_id,
+            text=txt,
+            parse_mode='HTML'
+        )
+        except:
+            messages.error(request, 'Форма не отправлена. Попробуйте позже.')
         return render(request, 'wedding/promo.html', context={
             'form': False,
             'text': _('Форма отправлена, спасибо! Администратор свяжется с Вами в ближайшее время'),
