34f99fbbe0f20e39bd4f3cff8ce6e6bbade48816
Bobur <you@example.com>
2022-04-13 09:54:20 +0500

    asdadas

--- tender/forms.py
+++ tender/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Tender
+from django.utils.translation import gettext_lazy as _
 
 
 class TenderCreateForm(forms.ModelForm):
+    on_date = forms.DateField(widget=forms.SelectDateWidget(), label=_("Дата события"))
     class Meta:
         model = Tender
         fields = ['service', 'on_date', 'budget', 'comment']
